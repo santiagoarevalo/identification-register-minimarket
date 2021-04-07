c97c39237928ec66890078423c4817fed6ab27b0
santiagoarevalo <santiguitar2003@gmail.com>
2021-04-06 20:48:00 -0500

    addPerson() method modified, NumberFormatException fixed

--- src/model/MiniMarket.java
+++ src/model/MiniMarket.java
@@ -3,6 +3,9 @@ package model;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import exceptions.IDNumberException;
+import exceptions.UnderAgeException;
+
 public class MiniMarket {
 	private final static int TI = 1;
 	private ArrayList<Person> people;
@@ -17,19 +20,20 @@ public class MiniMarket {
 		return contAttempts;
 	}
 
-	public boolean addPerson(int documentationType, int id) {
+	public boolean addPerson(int documentationType, int id) throws UnderAgeException, IDNumberException {
 		boolean added = false;
 		contAttempts++;
 		Person newPerson = new Person(documentationType, id);
 		String digit = Integer.toString(id);
-		digit = String.valueOf((digit.length()-2));
+		digit = digit.substring((digit.length()-2), (digit.length()-1));
 		int penultimate = Integer.parseInt(digit);
+		int dayOfMonth = LocalDate.now().getDayOfMonth();
 		if(!people.contains(newPerson)) {
-			if(documentationType != TI) {
-				//throw new UnderAgeException
+			if(documentationType == TI) {
+				throw new UnderAgeException();
 			}
-			else if((penultimate%2 == 0 && LocalDate.now().getDayOfMonth()%2 == 0) || (penultimate%2 != 0 && LocalDate.now().getDayOfMonth()%2 != 0)) {
-				//throw new Exception
+			else if((penultimate%2 == 0 && dayOfMonth%2 == 0) || (penultimate%2 != 0 && dayOfMonth%2 != 0)) {
+				throw new IDNumberException(dayOfMonth, penultimate);
 			}
 			added = people.add(newPerson);
 		}
