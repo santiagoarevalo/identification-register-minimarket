54f3a846e1137405c8d8becb7e8f63319d1f2ad7
santiagoarevalo <santiguitar2003@gmail.com>
2021-04-09 00:25:00 -0500

    Unit Tests implemented, getPeople() method added in MiniMarket Class

--- src/model/MiniMarket.java
+++ src/model/MiniMarket.java
@@ -40,4 +40,8 @@ public class MiniMarket {
 		
 		return added;
 	}
+
+	public ArrayList<Person> getPeople() {
+		return people;
+	}
 }
---
+++ test/model/MiniMarketTest.java
@@ -0,0 +1,79 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import exceptions.IDNumberException;
+import exceptions.UnderAgeException;
+
+public class MiniMarketTest {
+	private MiniMarket m;
+	
+	public void setupScenary1() {
+		m = new MiniMarket();
+	}
+	
+	@Test
+	public void testAddPerson1() throws UnderAgeException, IDNumberException {
+		setupScenary1();
+		
+		int type = 2;
+		int number = 1005865627;
+		
+		boolean added = m.addPerson(type, number);
+		assertTrue(added);
+		
+		ArrayList<Person> people = m.getPeople();
+		assertEquals(1, people.size());
+		
+		Person p = people.get(0);
+		assertEquals(type, p.getType());
+		assertEquals(number, p.getNumber());
+	}
+	
+	@Test
+	public void testAddPerson2() {
+		setupScenary1();
+		
+		int type = 1;
+		int number = 1002839617;
+		
+		boolean added = true;
+		try {
+			added = m.addPerson(type, number);
+		} catch (UnderAgeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IDNumberException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertFalse(added);
+		
+		ArrayList<Person> people = m.getPeople();
+		assertEquals(0, people.size());
+	}
+	
+	@Test
+	public void testAddPerson3() {
+		setupScenary1();
+		
+		int type = 3;
+		int number = 1008867657;
+		
+		boolean added = true;
+		try {
+			added = m.addPerson(type, number);
+		} catch (UnderAgeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IDNumberException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertFalse(added);
+		
+		ArrayList<Person> people = m.getPeople();
+		assertEquals(0, people.size());
+	}
+}
